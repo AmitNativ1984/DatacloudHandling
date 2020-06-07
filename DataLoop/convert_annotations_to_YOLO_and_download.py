c67bec64a9715266843dd5e72a65de9c64f0cab7
amit <amit.nativ@elbitsystems.com>
2020-06-07 16:59:46 +0300

    updating bounding box download

--- DataLoop/convert_annotations_to_YOLO_and_download.py
+++ DataLoop/convert_annotations_to_YOLO_and_download.py
@@ -9,7 +9,11 @@ logging.getLogger(__name__)
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="download annotations", fromfile_prefix_chars="@")
+    parser = argparse.ArgumentParser(description="convert_annotations_to_YOLO_and_download", fromfile_prefix_chars="@")
+
+    parser.add_argument('--project-name', type=str, default="Elbit",
+                        required=True,
+                        help='path to annoations on dataloop server')
 
     parser.add_argument('--dataloop-path', type=str,
                         required=True,
@@ -22,10 +26,10 @@ if __name__ == "__main__":
     args = parser.parse_args()
     print(args)
     # connect to dataloop server
-    establish_dataloop_connection()
+    establish_dataloop_connection(args)
 
     # upload the entire local input folder to dest location on dataloop server
-    project = dl.projects.get(project_name="Elbit")
+    project = dl.projects.get(project_name=args.project_name)
 
 
     try:
@@ -36,8 +40,18 @@ if __name__ == "__main__":
         dataset = project.datasets.create(dataset_name=args.dest_dataloop_path)
         logging.info("successfully created new dataset with following name: {}".format(args.dest_dataloop_path))
 
+    # downloading images
+    dataset.items.download(local_path=args.local_dest, annotation_options='box')
+
     # converting to yolo and downloading
     converter = dl.Converter()
+    filters = dl.Filters()
+    # filtering only boxes
+    filters.resource = dl.FiltersResource.ANNOTATION
+    filters.add(field='type', values='box')
+
+    # downloading labels and converting
     converter.convert_dataset(dataset=dataset,
                               to_format='yolo',
-                              local_path=args.local_dest)
+                              local_path=args.local_dest,
+                              annotation_filter=filters)
--- download_scripts.sh
+++ download_scripts.sh
@@ -1,3 +1,2 @@
 #!/bin/#!/usr/bin/env bash
-
-aws s3 cp s3://c4i-datasets/ben_shemen/14-01-2020/drive_cam0_Temp ~/Data/dataloop_annotations/14-01-2020/drive1-day --recursive
+python DataLoop/convert_annotations_to_YOLO_and_download.py --project-name Follow_Me --dataloop-path LabWindowBoundingBox --local-dest /home/amit/Data/Kzir/Windows/LabWindowsNewCam/
