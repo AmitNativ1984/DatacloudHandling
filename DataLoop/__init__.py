de93e0ac60920ca12ced7c09474870466d99d333
amit <amit.nativ@elbitsystems.com>
2020-06-07 17:03:12 +0300

    adding args to __init__.py

--- DataLoop/__init__.py
+++ DataLoop/__init__.py
@@ -5,15 +5,18 @@ logging.basicConfig(level=logging.INFO, format=FORMAT)
 logging.getLogger(__name__)
 
 class establish_dataloop_connection:
-    def __init__(self):
+    def __init__(self, args):
         self.Client_ID = 'hfmoGbcc7XxBmRz8ZenJmS0CADOuC95I'
         self.Client_secret = 'lFcRgAdiRo5uaxa-hVoIIGvRqZ8xiyrCjXqWBtUlVq1blKMfPmvE90bzPFoYosXP'
-        self.email= 'elbit@dataloop.ai'
-        self.Password = 'ElbitDatal@@p!'
+        # self.email= 'elbit@dataloop.ai'
+        # self.Password = 'ElbitDatal@@p!'
+
+        self.email = #PUT YOUR EMAIL HERE!
+        self.Password = #PUT YOUR PASSWORD HERE!
 
         dl.login_secret(self.email, self.Password, self.Client_ID, self.Client_secret)
         logging.info("successfully connected to dataloop server")
         dl.projects.list().print()
 
-        project = dl.projects.get(project_name="Elbit")
+        project = dl.projects.get(project_name=args.project_name)
         project.datasets.list().print()
