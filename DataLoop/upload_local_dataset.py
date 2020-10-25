40818c15239fd5b8c7a6938a78720e380a3ca842
amit <amit.nativ@elbitsystems.com>
2020-10-25 21:04:11 +0200

    cleanup

--- DataLoop/upload_local_dataset.py
+++ DataLoop/upload_local_dataset.py
@@ -18,13 +18,18 @@ if __name__ == "__main__":
                         required=True,
                         help='dest path inside dataloop project')
 
+    parser.add_argument('--n', type=int, default=1, help='take only 1 every <n> files')
+    parser.add_argument('--prefix', type=str, default='', help='string in file prefix')
+    parser.add_argument('--postfix', type=str, default='.bmp', help='string in file end')
+    parser.add_argument('--project-name', type=str, default='Elbit', help='dataloop project name')
+
     args = parser.parse_args()
     print(args)
     # connect to dataloop server
-    establish_dataloop_connection()
+    establish_dataloop_connection(args)
 
     # upload the entire local input folder to dest location on dataloop server
-    project = dl.projects.get(project_name="Elbit")
+    project = dl.projects.get(project_name=args.project_name)
 
 
     try:
@@ -36,15 +41,21 @@ if __name__ == "__main__":
         logging.info("successfully created new dataset with following name: {}".format(args.dest_dataloop_path))
 
     # go down tree of local dataset and upload all files. keeping tree structure
-    for curr_path in os.walk(args.source):
+    for path_ind, curr_path in enumerate(os.walk(args.source)):
         # check files do exist in current path
         files = curr_path[-1]
         if not files:
             continue
 
-        remote_subfolder = curr_path[0].split(args.source)[-1]
-        # upload all files in subfolder:
-        dataset.items.upload(local_path=curr_path[0])
-        logging.info("successfully updated all files from {}".format(curr_path[0]))
+        for n, file in enumerate(files):
+            if n % args.n != 0:
+                continue
+
+            if args.prefix not in file or args.postfix not in file:
+                continue
+
+            # upload all files in subfolder:
+            dataset.items.upload(local_path=os.path.join(curr_path[0], file))
+            logging.info("successfully updated all files from {}".format(curr_path[0]))
 
     logging.info("finished uploading files")
